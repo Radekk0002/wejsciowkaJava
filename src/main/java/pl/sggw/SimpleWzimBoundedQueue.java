12c0679a6c9dc7e59b29766dcadb3e9c46b073a4
Jakub Sitek <jakub.sitek@rkantor.com>
2021-11-24 01:49:49 +0100

    Dodanie interfejsu WzimBoundedQueue

---
+++ src/main/java/pl/sggw/SimpleWzimBoundedQueue.java
@@ -0,0 +1,12 @@
+package pl.sggw;
+
+import java.util.LinkedList;
+
+public class SimpleWzimBoundedQueue<E> implements WzimBoundedQueue<E> {
+
+
+    public SimpleWzimBoundedQueue(final int maxCapacity) {
+    }
+
+
+}
